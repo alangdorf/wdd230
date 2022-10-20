dc12e55b502d5d487980a1eebc45b06d9f0cef15
Allison Langdorf <alangdorf@yahoo.com>
2022-10-20 08:55:55 -0400

    chamber home page finished

--- chamber/scripts/currtime.js
+++ chamber/scripts/currtime.js
@@ -2,3 +2,8 @@ const dateDisplay = document.getElementById("currTime");
 const todaysDate = new Date;
 const options2 = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 dateDisplay.innerText = todaysDate.toLocaleDateString('en-US', options2);
+
+const banner = document.getElementById("banner");
+if (todaysDate.getDay() === 1 || todaysDate.getDay() === 2) {
+    banner.style.display = "block";
+}
