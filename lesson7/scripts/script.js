1a9aa5a278b3f1a733ea5372b6b46bab4fd673cf
Allison Langdorf <alangdorf@yahoo.com>
2022-10-29 20:50:23 -0400

    lazy loading

---
+++ lesson7/scripts/script.js
@@ -0,0 +1,33 @@
+const images = document.querySelectorAll("[data-src");
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if(!src) {
+        return;
+    } 
+    img.src = src;
+    img.onload = () => {
+        img.removeAttribute("data-src");
+    }
+} 
+
+if ("IntersectionObserver" in window) {
+    const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+        entries.forEach(entry => {
+            if(!entry.isIntersecting) {
+                return;
+            } else {
+                preloadImage(entry.target);
+                imgObserver.unobserve(entry.target);
+            }
+        });
+    });
+
+    images.forEach(image => {
+        imgObserver.observe(image);
+    });
+} else {
+    images.forEach(image => {
+        preloadImage(image);
+    });
+}
